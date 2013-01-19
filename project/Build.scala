6dc3a53ddbbdd2550ff8d1663981e32f2adf8035
Michael Korbakov <rmihael@gmail.com>
2013-01-19 04:37:10 +0200

    Global restructuring into multiple projects

--- project/Build.scala
+++ project/Build.scala
@@ -1,23 +1,56 @@
 import sbt._
 import Keys._
 import PlayProject._
+import com.typesafe.startscript.StartScriptPlugin
+
+object BuildSettings {
+  import Dependencies._
+  import Resolvers._
+
+  val globalSettings = Seq(
+    scalacOptions += "-deprecation",
+    libraryDependencies ++= Seq(jodaTime),
+    resolvers := Seq(typesafeRepo))
+
+  val projectSettings = Defaults.defaultSettings ++ globalSettings
+}
+
+object Resolvers {
+  val typesafeRepo = "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
+}
+
+object Dependencies {
+  val jodaTime = "org.scalaj" %% "scalaj-time" % "0.6"
+  val akkaHttp = "com.typesafe.akka" % "akka-actor" % "2.0.5"
+  val playFramework = "play" %% "play" % play.core.PlayVersion.current
+}
 
 object ApplicationBuild extends Build {
-  override def settings = super.settings ++ org.sbtidea.SbtIdeaPlugin.ideaSettings
+  import Dependencies._
+  import BuildSettings._
 
-  val appName         = "mmmtg-server"
+  override def settings = super.settings ++ org.sbtidea.SbtIdeaPlugin.ideaSettings ++ globalSettings
+
+  val appName         = "mmmtg"
   val appVersion      = "1.0-SNAPSHOT"
 
-  val appDependencies = Seq(
-    "play" %% "play" % play.core.PlayVersion.current,
-    "org.scalaj" %% "scalaj-time" % "0.6"
+  val commons = Project("commons", file("commons"),
+                        settings = projectSettings ++
+                                   Seq(libraryDependencies ++= Seq(playFramework, jodaTime))
   )
 
-  val commons = Project("commons", file("commons")).settings(
-    libraryDependencies ++= appDependencies
-  )
-
-  val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+  val server = PlayProject("server", appVersion, Seq(jodaTime), path=file("server"), mainLang=SCALA).settings(
     // Add your own project settings here
-  ).dependsOn(commons).aggregate(commons)
+  ) dependsOn(commons)
+
+  val indexer = Project("indexer", file("indexer"),
+                        settings=projectSettings ++
+                                 StartScriptPlugin.startScriptForClassesSettings ++
+                                 Seq(libraryDependencies ++= Seq(akkaHttp, jodaTime))
+  ) dependsOn(commons)
+
+  val main = Project(appName, file("."),
+                     settings = projectSettings ++
+                                Seq(StartScriptPlugin.stage in Compile := Unit)
+  ) aggregate(commons, server, indexer)
 }
