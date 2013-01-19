2bf583b71e3cc3caf5ccb812f6c7944215465400
Michael Korbakov <rmihael@gmail.com>
2013-01-19 03:01:43 +0200

    Split up models to commons subproject

--- project/Build.scala
+++ project/Build.scala
@@ -3,16 +3,21 @@ import Keys._
 import PlayProject._
 
 object ApplicationBuild extends Build {
+  override def settings = super.settings ++ org.sbtidea.SbtIdeaPlugin.ideaSettings
 
-    val appName         = "mmmtg-server"
-    val appVersion      = "1.0-SNAPSHOT"
+  val appName         = "mmmtg-server"
+  val appVersion      = "1.0-SNAPSHOT"
 
-    val appDependencies = Seq(
-      "org.scalaj" %% "scalaj-time" % "0.6"
-    )
+  val appDependencies = Seq(
+    "play" %% "play" % play.core.PlayVersion.current,
+    "org.scalaj" %% "scalaj-time" % "0.6"
+  )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
-    )
+  val commons = Project("commons", file("commons")).settings(
+    libraryDependencies ++= appDependencies
+  )
 
+  val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+    // Add your own project settings here
+  ).dependsOn(commons).aggregate(commons)
 }
